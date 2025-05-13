a91d6b5f7ade54f58d172c9890c62d12c089172b
Nicolas Marques <nimarques1207@gmail.com>
2025-05-13 10:51:27 -0300

    php

--- frontend/scripts/updateSensors.js
+++ frontend/scripts/updateSensors.js
@@ -2,13 +2,19 @@ const apiBaseUrl = "http://localhost:8080/api/sensors";
 
 async function fetchLatestSensorData() {
     try {
-        const response = await fetch(`${apiBaseUrl}/latest`);
+        const response = await fetch(`${apiBaseUrl}`);
         if (!response.ok) {
             throw new Error(`Erro ao buscar dados: ${response.status}`);
         }
         const data = await response.json();
+
         console.log(data)
-        updateSensorDisplay(data);
+
+        // Pega o último registro do array
+        const latestData = data[data.length - 1];
+        console.log(latestData);
+
+        updateSensorDisplay(latestData);
     } catch (error) {
         console.error("Erro ao buscar os dados mais recentes:", error);
     }
@@ -17,9 +23,9 @@ async function fetchLatestSensorData() {
 function updateSensorDisplay(data) {
     const sensorDataElements = document.querySelectorAll(".sensor-data");
     if (sensorDataElements.length >= 3) {
-        sensorDataElements[0].textContent = `${data.luminosidade || 0}%`; // Luminosidade
-        sensorDataElements[1].textContent = `${data.temperatura || 0}°C`; // Temperatura
-        sensorDataElements[2].textContent = `${data.umidade || 0}%`; // Umidade
+        sensorDataElements[0].textContent = `${data.luminosidade}%`; // Luminosidade
+        sensorDataElements[1].textContent = `${data.temperatura}°C`; // Temperatura
+        sensorDataElements[2].textContent = `${data.umidade}%`; // Umidade
     } else {
         console.error("Elementos .sensor-data não encontrados no DOM.");
     }
